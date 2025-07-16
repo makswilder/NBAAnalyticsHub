4f64d8ccd780363a3995c0d47721eb4daa95f5e3
makswilder <maksim.shcherbakov.sh@gmail.com>
2025-07-16 16:12:45 +0200

    refactor(player): renamed field 'pos' to 'position' for clarity

--- backend/src/main/java/com/nba/analyticshub/domain/entity/Player.java
+++ backend/src/main/java/com/nba/analyticshub/domain/entity/Player.java
@@ -29,7 +29,7 @@ public class Player {
     private Integer age;
 
     @Column(name = "position")
-    private String pos;
+    private String position;
 
     @Column(name = "games_played")
     private Integer g;
@@ -71,11 +71,11 @@ public class Player {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Player player = (Player) o;
-        return Objects.equals(id, player.id) && Objects.equals(name, player.name) && Objects.equals(nation, player.nation) && Objects.equals(age, player.age) && Objects.equals(pos, player.pos) && Objects.equals(g, player.g) && Objects.equals(gs, player.gs) && Objects.equals(mp, player.mp) && Objects.equals(fg, player.fg) && Objects.equals(threeP, player.threeP) && Objects.equals(twoP, player.twoP) && Objects.equals(ast, player.ast) && Objects.equals(stl, player.stl) && Objects.equals(blk, player.blk) && Objects.equals(tov, player.tov) && Objects.equals(pts, player.pts) && Objects.equals(team, player.team);
+        return Objects.equals(id, player.id) && Objects.equals(name, player.name) && Objects.equals(nation, player.nation) && Objects.equals(age, player.age) && Objects.equals(position, player.position) && Objects.equals(g, player.g) && Objects.equals(gs, player.gs) && Objects.equals(mp, player.mp) && Objects.equals(fg, player.fg) && Objects.equals(threeP, player.threeP) && Objects.equals(twoP, player.twoP) && Objects.equals(ast, player.ast) && Objects.equals(stl, player.stl) && Objects.equals(blk, player.blk) && Objects.equals(tov, player.tov) && Objects.equals(pts, player.pts) && Objects.equals(team, player.team);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, nation, age, pos, g, gs, mp, fg, threeP, twoP, ast, stl, blk, tov, pts, team);
+        return Objects.hash(id, name, nation, age, position, g, gs, mp, fg, threeP, twoP, ast, stl, blk, tov, pts, team);
     }
 }
