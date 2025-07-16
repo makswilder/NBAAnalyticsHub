bf1fb560f28de213ed36916e937f1c3347188514
Kantor Istvan <kantoristvan17@gmail.com>
2025-07-16 14:49:04 +0200

    sign-up-page

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Button, TextField, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-function Login() {
+function Register() {
   return (
     <Box
       sx={{
@@ -15,24 +15,25 @@ function Login() {
       }}
     >
       <Typography variant='h4' gutterBottom>
-        Login
+        Sign up
       </Typography>
+      <TextField label='Name' type='text' fullWidth margin='normal' />
       <TextField label='Email' type='email' fullWidth margin='normal' />
       <TextField label='Password' type='password' fullWidth margin='normal' />
       <Button variant='contained' color='primary' fullWidth sx={{ mt: 2 }}>
-        Login
+        Sign up
       </Button>
       <Box sx={{ my: 2 }}>
         <hr style={{ border: 'none', borderTop: '1px solid #ccc' }} />
       </Box>
       <Typography variant='body2' sx={{ textAlign: 'center' }}>
-        Don't have an account?{' '}
-        <Button color='primary' component={Link} to='/register'>
-          Register
+        Already have an account?{' '}
+        <Button color='primary' component={Link} to='/login'>
+          Login
         </Button>
       </Typography>
     </Box>
   );
 }
 
-export default Login;
+export default Register;
