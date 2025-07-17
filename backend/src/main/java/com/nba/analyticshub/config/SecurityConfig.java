a68ad2739fa6abf1e3f56bb4f202d91113d046b6
makswilder <maksim.shcherbakov.sh@gmail.com>
2025-07-17 15:38:52 +0200

    feat(security): registered JwtAuthFilter and preload default user

--- backend/src/main/java/com/nba/analyticshub/config/SecurityConfig.java
+++ backend/src/main/java/com/nba/analyticshub/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.nba.analyticshub.config;
 
+import com.nba.analyticshub.domain.entity.User;
+import com.nba.analyticshub.repository.UserRepository;
+import com.nba.analyticshub.security.JwtAuthFilter;
+import com.nba.analyticshub.security.NbaUserDetailsService;
+import com.nba.analyticshub.service.AuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,18 +14,45 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthFilter jwtAuthFilter(AuthService authService) {
+        return new JwtAuthFilter(authService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        NbaUserDetailsService nbaUserDetailsService = new NbaUserDetailsService(userRepository);
+
+        String email = "user1@test.com";
+        userRepository.findByEmail(email).orElseGet(() -> {
+            User newUser = User.builder()
+                    .name("Test User")
+                    .email(email)
+                    .password(passwordEncoder().encode("password"))
+                    .build();
+            return userRepository.save(newUser);
+        });
+
+        return  nbaUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            JwtAuthFilter jwtAuthFilter) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/players/**").permitAll()
                         .anyRequest().authenticated()
@@ -28,7 +60,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
